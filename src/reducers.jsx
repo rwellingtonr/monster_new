b65d46a023c0061072c7bc8bbada0e2eab948ab5
rwellingtonr <wellington-158@hotmail.com>
2021-08-01 17:10:54 -0300

    added comments to reducers.jsx

--- src/reducers.jsx
+++ src/reducers.jsx
@@ -1,9 +1,11 @@
 import { CHANGE_SEARCHFIELD } from "./constants.jsx"
 
+//the initial state
 const initialState = {
   searchField: "",
 }
 
+// Switch through the actions type to do a specifical function
 export const searchMonsters = (state = initialState, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCHFIELD:
